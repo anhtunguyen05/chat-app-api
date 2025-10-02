f698aeb220428fee86a141738a0f2f54d2f76fc9
tudeptrai <anhtunguyen643@gmail.com>
2025-10-02 22:23:54 +0700

    refactor/refactor access token & refresh token #13

--- src/app/middleware/auth.middleware.ts
+++ src/app/middleware/auth.middleware.ts
@@ -1,10 +1,14 @@
 import { Request, Response, NextFunction } from "express";
 import jwt, { JwtPayload } from "jsonwebtoken";
 
-// 🔹 Mở rộng interface Request để có thể gắn user vào req
+interface AuthPayload extends JwtPayload {
+  id: string;
+}
+
+// 🔹 Mở rộng interface Request để có thể gắn userId vào req
 declare module "express-serve-static-core" {
   interface Request {
-    user?: string | JwtPayload;
+    id?: string;
   }
 }
 
@@ -13,7 +17,7 @@ export function verifyToken(
   res: Response,
   next: NextFunction
 ): void {
-  const token = req.cookies?.accessToken as string | undefined; // token trong cookie httpOnly
+  const token = req.cookies?.accessToken as string | undefined;
 
   if (!token) {
     res.status(401).json({ message: "Không có token" });
@@ -21,8 +25,8 @@ export function verifyToken(
   }
 
   try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET as string);
-    req.user = decoded; // gắn user id hoặc payload vào req
+    const decoded = jwt.verify(token, process.env.JWT_SECRET as string) as AuthPayload;
+    req.id = decoded.id; // ✅ gắn userId vào req
     next();
   } catch (err) {
     res.status(401).json({ message: "Token không hợp lệ hoặc đã hết hạn" });
